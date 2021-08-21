456e72a0afb484a61e219f3337ab726edd5fa944
RaulDuque <raulgduque2000@gmail.com>
2021-08-20 22:55:03 -0300

    4 tabelas

---
+++ database/migrations/2021_08_21_011443_create_addresses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('street');
+            $table->integer('number',11);
+            $table->string('cep',8);
+            $table->string('city');
+            $table->string('state',2);
+            $table->string('complement');
+            $table->string('addressable_type');
+            $table->bigInteger('addressable_id',2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
---
+++ database/migrations/2021_08_21_011623_create_contacts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('phone_number', 11);
+            $table->tinyInteger('whatsapp', 1);
+            $table->string('contactable_type');
+            $table->bigInteger('contactable_id', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
